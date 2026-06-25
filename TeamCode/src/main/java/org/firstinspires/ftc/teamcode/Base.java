bc5caf56444b32a4def13805c1fe732fd15ebbe6
parthgoyal <epicparth@gmail.com>
2026-06-24 20:33:05 -0400

    Test Push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Base {
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.List;
+
+public abstract class Base extends LinearOpMode {
+    public List<LynxModule> allHubs;
+    public void initHardware(){
+
+    }
+
+    public void resetCache(){
+        for (LynxModule hub : allHubs) {
+            hub.clearBulkCache();
+        }
+    }
 }
